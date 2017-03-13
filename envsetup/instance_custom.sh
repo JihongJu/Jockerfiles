1f8abd604a6129fa5c5749d3af717fb0097b6a03
jihongju <daniel.jihong.ju@gmail.com>
2017-03-13 12:08:00 +0100

    Fix typo

---
+++ envsetup/instance_custom.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+# Custom VIM/Tmux settings for Jihong
+#
+# ============================================================ #
+
+echo "Writing .vim_runtime/my_configs.vim"
+wget "https://raw.githubusercontent.com/JihongJu/Jockerfiles/master/envsetup/my_configs.vim" -O ${HOME}/.vim_runtime/my_configs.vim
+
+
+echo "Writing .tmux.conf"
+wget "https://raw.githubusercontent.com/JihongJu/Jockerfiles/master/envsetup/tmux.conf" -O ${HOME}/.tmux.conf
+
+
+echo "Adding .tmux.completion.bash to bashrc"
+wget "https://raw.githubusercontent.com/JihongJu/Jockerfiles/master/envsetup/tmux.completion.bash" -O ${HOME}/.tmux.completion.bash
+grep -q -F 'source ${HOME}/.tmux.completion.bash' ${HOME}/.bashrc || echo 'source ${HOME}/.tmux.completion.bash' >> ${HOME}/.bashrc
+source ${HOME}/.bashrc
